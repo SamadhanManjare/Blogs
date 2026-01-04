e5316faa62b5f74c0e9178a8ca29097f3bcf5dd6
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-01-04 18:53:27 +0530

    header updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
+    // Header component with fixed position, full width, centered title, bottom border, and shadow
     <div className='fixed top-0 inset-x-0 w-full text-center border-b-2 border-gray-300 shadow-md bg-white py-4'>
       <header className=''>
         <h1 className='font-bold text-3xl uppercase text-center'>Blogs</h1>

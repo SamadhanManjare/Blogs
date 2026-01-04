b74b3f059846734146b8657f3910d94a6f9aaad3
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-01-04 18:57:14 +0530

    pagination file updated

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -4,6 +4,7 @@ import { AppContext } from "../Context/AppContext";
 const Pagination = () => {
   const { page, totalPages, handlerPageChange } = useContext(AppContext);
   return (
+    // Pagination component with fixed position at bottom, full width, top border, and navigation buttons
     <div className="  fixed bottom-0 bg-white py-2 border-t-2 border-t-gray-300 w-full inset-x-0">
       <div className="flex gap-x-3 items-center w-11/12 max-w-2xl mx-auto">
         {page > 1 && (
